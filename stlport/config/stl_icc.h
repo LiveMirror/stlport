12b89df9edcd26a3b4080212df69513265e77180
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2003-10-06 18:35:40 +0000

    no message
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/R451_dev/STLport@359 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/config/stl_icc.h
+++ stlport/config/stl_icc.h
@@ -8,9 +8,16 @@
 // compiler headers included. Default is "../include".
 // C headers may reside in different directory, so separate macro is provided.
 # if (__INTEL_COMPILER >= 800)
+
 # define _STLP_NATIVE_INCLUDE_PATH ../include/c++
 # define _STLP_NATIVE_C_INCLUDE_PATH ../include
 # define _STLP_NATIVE_CPP_C_INCLUDE_PATH ../include
+
+# ifndef __GNUC__ 
+//  If GCC compatability is diabled (-no-gcc is specified) STD needs to be redefined.
+# define _STLP_REDEFINE_STD 1
+# endif /* __GNUC__ */
+
 # endif /* __INTEL_COMPILER >= 800 */
 
 # undef _STLP_WINCE
