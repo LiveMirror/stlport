7ef0de6d16c28d4ed7503cb4512d8e029ec36d7e
fbp <fbp@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-01-14 18:05:32 +0000

    A recursively-safe mutex used
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_4_6_patch/STLport@492 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/debug/_debug.h
+++ stlport/stl/debug/_debug.h
@@ -336,7 +336,7 @@ public:
   }
   
   mutable __owned_link              _M_node; 
-  mutable _STLP_mutex               _M_lock;
+  mutable _STLP_mutex_RS            _M_lock;
   
 private:
   // should never be called, should be left undefined,
