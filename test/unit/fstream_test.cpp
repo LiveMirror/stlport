b4fe0e41293cb63f073f9cf40c6f0a0dc4178bee
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-07-07 11:43:27 +0000

    test for correct report by tellg added
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@828 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/fstream_test.cpp
+++ test/unit/fstream_test.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 #include "cppunit/cppunit_proxy.h"
 
@@ -18,6 +19,7 @@ class FstreamTest : public CPPUNIT_NS::TestCase
   CPPUNIT_TEST(input);
   CPPUNIT_TEST(io);
   CPPUNIT_TEST(err);
+  CPPUNIT_TEST(tellg);
   CPPUNIT_TEST_SUITE_END();
 
   protected:
@@ -25,6 +27,7 @@ class FstreamTest : public CPPUNIT_NS::TestCase
     void input();
     void io();
     void err();
+    void tellg();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FstreamTest);
@@ -119,4 +122,32 @@ void FstreamTest::err()
   CPPUNIT_ASSERT( f.fail() );
   CPPUNIT_ASSERT( f.eof() );
   CPPUNIT_ASSERT( i == 9 );
+}
+
+void FstreamTest::tellg()
+{
+  // bogus ios_base::binary is for Wins
+  ofstream of( "test_file.txt", ios_base::out | ios_base::binary | ios_base::trunc );
+  CPPUNIT_ASSERT( of.is_open() );
+  int i;
+
+  for ( i = 0; i < 50; ++i ) {
+    of << "line " << setiosflags(ios_base::right) << setfill('0') << setw(2) << i << "\n";
+  }
+  of.close();
+  // bogus ios_base::binary is for Wins
+  ifstream is( "test_file.txt", ios_base::in | ios_base::binary );
+  CPPUNIT_ASSERT( is.is_open() );
+  string line;
+  char buf[64];
+
+  // CPPUNIT_ASSERT( is.tellg() == 0 );
+  ifstream::streampos p = 0;
+  for ( i = 0; i < 50; ++i ) {
+    CPPUNIT_ASSERT( is.tellg() == p );
+    // getline( is, line, '\n' );
+    is.read( buf, 8 );
+    CPPUNIT_ASSERT( !is.fail() );
+    p += 8;
+  }
 }
