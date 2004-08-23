83c1779aeb020a88f7a1ec03af1c30b5993666bf
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2004-08-23 13:01:05 +0000

    CPPUNIT_MINI removed
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@905 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/cppunit/cppunit_proxy.h
+++ test/unit/cppunit/cppunit_proxy.h
@@ -18,12 +18,7 @@
 #ifndef _CPPUNITPROXYINTERFACE_H_
 # define _CPPUNITPROXYINTERFACE_H_
 
-#if defined(CPPUNIT_MINI) || defined(CPPUNIT_DUMMY)
-#   include "cppunit_mini.h"
-#else
-#   include <cppunit/TestCase.h>
-#   include <cppunit/extensions/HelperMacros.h>
-#endif
+# include "cppunit_mini.h"
 
 #endif
 
