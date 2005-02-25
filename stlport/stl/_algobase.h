9cb0c231024a4ae19eaf34b7fb7356f942628f95
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2005-02-25 07:21:16 +0000

    wrong parameters order in call to memchr
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@1278 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_algobase.h
+++ stlport/stl/_algobase.h
@@ -584,11 +584,11 @@ _InputIter find(_InputIter __first, _InputIter __last, const _Tp& __val);
 #if !defined (__BORLANDC__)
 inline char* 
 find(char* __first, char* __last, int __val) {
-  return __STATIC_CAST(char*, memchr(__first, __last - __first, __val));
+  return __STATIC_CAST(char*, memchr(__first, __val, __last - __first));
 }
 inline const char* 
 find(const char* __first, const char* __last, int __val) {
-  return __STATIC_CAST(const char*, memchr(__first, __last - __first, __val));
+  return __STATIC_CAST(const char*, memchr(__first, __val, __last - __first));
 }
 #endif
 
