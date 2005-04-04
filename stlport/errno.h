3d812b6ae8905a49605df525b830f06bc5ae5367
ptr <ptr@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2005-04-04 09:39:27 +0000

    evc3 and evc4 has no this native header
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@1412 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/errno.h
+++ stlport/errno.h
@@ -20,18 +20,21 @@
 #  define _STLP_DONT_POP_HEADER_ID
 #endif
 
-#if defined (errno)
+#ifdef _STLP_WCE
+# pragma message("eMbedded Visual C++ 3 and .NET don't have a errno.h header; STLport won't include native errno.h here")
+#else
+# if defined (errno)
 /* We are forced to undef errno otherwise the include of the native
  * errno.h file would fail with a message like:
  * unable to find file ../include/(errno_macro_replacement).h
  */
 #  define _STLP_ERRNO_ALREADY_DEFINED
 #  undef errno
-#endif
+# endif
 
-#include _STLP_NATIVE_C_HEADER(errno.h)
+# include _STLP_NATIVE_C_HEADER(errno.h)
 
-#if defined (errno) || defined (_STLP_ERRNO_ALREADY_DEFINED)
+# if defined (errno) || defined (_STLP_ERRNO_ALREADY_DEFINED)
 #  if !defined (_STLP_ERRNO_ALREADY_DEFINED)
 /* If errno was already defined it means that STLport errno.h
  * has already been included and __stlp_errno define. We do not 
@@ -45,11 +48,13 @@ inline int* __stlp_errno() {
 #  endif
 #  undef errno
 #  define errno (*__stlp_errno())
-#else
+# else
 _STLP_BEGIN_NAMESPACE
 using ::errno;
 _STLP_END_NAMESPACE
-#endif
+# endif
+#endif // _STLP_WCE
+
 
 #if (_STLP_OUTERMOST_HEADER_ID == 0x205)
 #  if ! defined (_STLP_DONT_POP_HEADER_ID)
