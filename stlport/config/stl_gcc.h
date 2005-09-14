f55e0354591088307385b855f21427df312bd948
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2005-09-14 13:29:18 +0000

    Activate Mingw string wide function support.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@1724 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/config/stl_gcc.h
+++ stlport/config/stl_gcc.h
@@ -129,7 +129,9 @@
 #  if !defined (__MINGW32__) && !defined (__CYGWIN__)
 #    define _STLP_NO_NATIVE_MBSTATE_T    1
 #  endif
-#  define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+#  if !defined (__MINGW32__) || (__GNUC__ < 3) || (__GNUC__ == 3) && (__GNUC_MINOR__ < 4)
+#    define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+#  endif
 #  define _STLP_NO_NATIVE_WIDE_STREAMS   1
 #elif defined(__linux__)
 #  if (__GNUC__ < 3)
