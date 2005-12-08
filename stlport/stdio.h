e97695fcd741c91729cdb1e5769c689ea2200bd4
complement <complement@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2005-12-08 20:09:35 +0000

    forward-declaration for _exception struct; useful for WinCE/eVC3
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@1939 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stdio.h
+++ stlport/stdio.h
@@ -27,6 +27,9 @@
 #  endif
 
 #  if !defined (_STLP_WINCE)
+#    if defined(_STLP_WCE_EVC3)
+struct _exception;
+#    endif
 #    include _STLP_NATIVE_C_HEADER(stdio.h)
 
 #    if defined (__SUNPRO_CC) && !defined (_STRUCT_FILE)
