91f94c81a01edd5885e3e73ec2f045ff0e8b62a6
complement <complement@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-01-12 20:45:33 +0000

    pass CharConsumer via reference, to take into account CharConsumer modifications. This has relation to find(char) in rope.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_0/STLport@2090 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- test/unit/rope_test.cpp
+++ test/unit/rope_test.cpp
@@ -15,10 +15,14 @@ class RopeTest : public CPPUNIT_NS::TestCase
 {
   CPPUNIT_TEST_SUITE(RopeTest);
   CPPUNIT_TEST(io);
+  CPPUNIT_TEST(find1);
+  CPPUNIT_TEST(find2);
   CPPUNIT_TEST_SUITE_END();
 
 protected:
   void io();
+  void find1();
+  void find2();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(RopeTest);
@@ -38,6 +42,24 @@ void RopeTest::io()
     CPPUNIT_ASSERT( ostr );
     CPPUNIT_ASSERT( ostr.str() == cstr );
   }
+}
+
+void RopeTest::find1()
+{
+  crope r("Fuzzy Wuzzy was a bear");
+  crope::size_type n = r.find( "hair" );
+  CPPUNIT_ASSERT( n == crope::npos );
+
+  n = r.find("ear");
+
+  CPPUNIT_ASSERT( n == (r.size() - 3) );
+}
+
+void RopeTest::find2()
+{
+  crope r("Fuzzy Wuzzy was a bear");
+  crope::size_type n = r.find( 'e' );
+  CPPUNIT_ASSERT( n == (r.size() - 3) );
 }
 
 #endif
