138e78d0818c301fb681b7e077f1b876168f2970
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-09-20 13:28:56 +0000

    basic_filebuf::seekoff fix for codecvt with variable encoding.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_1/STLport@2603 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_fstream.c
+++ stlport/stl/_fstream.c
@@ -290,7 +290,7 @@ basic_filebuf<_CharT, _Traits>::seekoff(off_type __off,
       return _M_seek_return(_M_base._M_seek(_M_width * __off, __whence),
                             _State_type());
 
-    // Seek relative to current position.  Complicated if we're in input mode.
+    // Seek relative to current position. Complicated if we're in input mode.
     else if (__whence == ios_base::cur) {
       if (!_M_in_input_mode)
         return _M_seek_return(_M_base._M_seek(_M_width * __off, __whence),
@@ -301,9 +301,8 @@ basic_filebuf<_CharT, _Traits>::seekoff(off_type __off,
         streamoff __adjust = _M_mmap_len - (this->gptr() - (_CharT*) _M_mmap_base);
 
         // if __off == 0, we do not need to exit input mode and to shift file pointer
-        return __off == 0 ?
-          pos_type(_M_base._M_seek(0, ios_base::cur) - __adjust) :
-          _M_seek_return(_M_base._M_seek(__off - __adjust, ios_base::cur), _State_type());
+        return __off == 0 ? pos_type(_M_base._M_seek(0, ios_base::cur) - __adjust)
+                          : _M_seek_return(_M_base._M_seek(__off - __adjust, ios_base::cur), _State_type());
       }
       else if (_M_constant_width) { // Get or set the position.
         streamoff __iadj = _M_width * (this->gptr() - this->eback());
@@ -316,9 +315,8 @@ basic_filebuf<_CharT, _Traits>::seekoff(off_type __off,
         if (__iadj <= _M_ext_buf_end - _M_ext_buf) {
           streamoff __eadj =  _M_base._M_get_offset(_M_ext_buf + __STATIC_CAST(ptrdiff_t, __iadj), _M_ext_buf_end);
 
-          return __off == 0 ?
-            pos_type(_M_base._M_seek(0, ios_base::cur) - __eadj) :
-            _M_seek_return(_M_base._M_seek(__off - __eadj, ios_base::cur), _State_type());
+          return __off == 0 ? pos_type(_M_base._M_seek(0, ios_base::cur) - __eadj)
+                            : _M_seek_return(_M_base._M_seek(__off - __eadj, ios_base::cur), _State_type());
         }
       } else {                    // Get the position.  Encoding is var width.
         // Get position in internal buffer.
@@ -329,32 +327,35 @@ basic_filebuf<_CharT, _Traits>::seekoff(off_type __off,
         int __epos = _M_codecvt->length(__state, _M_ext_buf, _M_ext_buf_end,
                                         __ipos);
 
-        // Sanity check (expensive): make sure __epos is the right answer.
-        _State_type __tmp_state = _M_state;
-        _Filebuf_Tmp_Buf<_CharT> __buf(__ipos);
-        _CharT* __ibegin = __buf._M_ptr;
-        _CharT* __inext  = __ibegin;
+        if (__epos >= 0) {
+          // Sanity check (expensive): make sure __epos is the right answer.
+          _State_type __tmp_state = _M_state;
+          _Filebuf_Tmp_Buf<_CharT> __buf(__ipos);
+          _CharT* __ibegin = __buf._M_ptr;
+          _CharT* __inext  = __ibegin;
 
-        const char* __dummy;
-        typename _Codecvt::result __status
-          = _M_codecvt->in(__tmp_state,
-                           _M_ext_buf, _M_ext_buf + __epos, __dummy,
-                           __ibegin, __ibegin + __ipos, __inext);
-        if (__status != _Codecvt::error &&
-            (__status == _Codecvt::noconv ||
-             (__inext == __ibegin + __ipos &&
-              equal(this->gptr(), this->eback(), __ibegin,
-                    _STLP_PRIV _Eq_traits<traits_type>())))) {
-          // Get the current position (at the end of the external buffer),
-          // then adjust it.  Again, it might be a text-oriented stream.
-          streamoff __cur = _M_base._M_seek(0, ios_base::cur);
-          streamoff __adj =
-            _M_base._M_get_offset(_M_ext_buf, _M_ext_buf + __epos) -
-            _M_base._M_get_offset(_M_ext_buf, _M_ext_buf_end);
-          if (__cur != -1 && __cur + __adj >= 0)
-            return _M_seek_return(__cur + __adj, __state);
+          const char* __dummy;
+          typename _Codecvt::result __status
+            = _M_codecvt->in(__tmp_state,
+                             _M_ext_buf, _M_ext_buf + __epos, __dummy,
+                             __ibegin, __ibegin + __ipos, __inext);
+          if (__status != _Codecvt::error &&
+              (__status == _Codecvt::noconv ||
+               (__inext == __ibegin + __ipos &&
+                equal(this->eback(), this->gptr(), __ibegin, _STLP_PRIV _Eq_traits<traits_type>())))) {
+            // Get the current position (at the end of the external buffer),
+            // then adjust it.  Again, it might be a text-oriented stream.
+            streamoff __cur = _M_base._M_seek(0, ios_base::cur);
+            streamoff __adj =
+              _M_base._M_get_offset(_M_ext_buf, _M_ext_buf + __epos) -
+              _M_base._M_get_offset(_M_ext_buf, _M_ext_buf_end);
+            if (__cur != -1 && __cur + __adj >= 0)
+              return __off == 0 ? pos_type(__cur + __adj)
+                                : _M_seek_return(__cur + __adj, __state);
+              //return _M_seek_return(__cur + __adj, __state);
+          }
+          // We failed the sanity check here.
         }
-        // We failed the sanity check here.
       }
     }
     // Unrecognized value for __whence here.
@@ -412,7 +413,7 @@ void basic_filebuf<_CharT, _Traits>::imbue(const locale& __loc) {
 // immediately after a seek.
 template <class _CharT, class _Traits>
 bool basic_filebuf<_CharT, _Traits>::_M_switch_to_input_mode() {
-  if (this->is_open() && (((int)_M_base.__o_mode() & (int)ios_base::in) !=0)
+  if (this->is_open() && (((int)_M_base.__o_mode() & (int)ios_base::in) != 0)
       && (_M_in_output_mode == 0) && (_M_in_error_mode == 0)) {
     if (!_M_int_buf && !_M_allocate_buffers())
       return false;
@@ -622,7 +623,7 @@ bool basic_filebuf<_CharT, _Traits>::_M_allocate_buffers(_CharT* __buf, streamsi
     _M_int_buf_dynamic = false;
   }
 
-  streamsize __ebufsiz = (max)(__n * __STATIC_CAST(streamsize, (max)(_M_codecvt->encoding(), 1)),
+  streamsize __ebufsiz = (max)(__n * __STATIC_CAST(streamsize, _M_width),
                                __STATIC_CAST(streamsize, _M_codecvt->max_length()));
   _M_ext_buf = 0;
   if ((sizeof(streamsize) < sizeof(size_t)) ||
