09f0c324fec8b50d0760272f2b2be9c442e2435e
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2006-10-06 20:14:23 +0000

    Fixed __debug_alloc implementation to avoid unjustified wrong deallocation size assertion.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_1/STLport@2640 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- stlport/stl/_alloc.c
+++ stlport/stl/_alloc.c
@@ -35,7 +35,7 @@
 _STLP_BEGIN_NAMESPACE
 
 template <class _Alloc>
-void * _STLP_CALL __debug_alloc<_Alloc>::allocate(size_t& __n) {
+void * _STLP_CALL __debug_alloc<_Alloc>::allocate(size_t __n) {
   size_t __total_extra = __extra_before_chunk() + __extra_after_chunk();
   size_t __real_n = __n + __total_extra;
   if (__real_n < __n) {
@@ -45,7 +45,6 @@ void * _STLP_CALL __debug_alloc<_Alloc>::allocate(size_t& __n) {
   }
   __alloc_header *__result = (__alloc_header *)__allocator_type::allocate(__real_n);
   memset((char*)__result, __shred_byte, __real_n * sizeof(value_type));
-  __n = __real_n - __total_extra;
   __result->__magic = __magic;
   __result->__type_size = sizeof(value_type);
   __result->_M_size = (_STLP_UINT32_T)__n;
