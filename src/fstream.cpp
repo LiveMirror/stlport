d439d4a9e24107448b4cddd5378495200655fe0b
dums <dums@01b27279-081b-0410-8cf9-d9d9e0fb0389>
2008-01-02 20:53:19 +0000

    #1854345 bug fixed.
    git-svn-id: https://stlport.svn.sourceforge.net/svnroot/stlport/branches/STLPORT_5_1/STLport@3359 01b27279-081b-0410-8cf9-d9d9e0fb0389

--- src/fstream.cpp
+++ src/fstream.cpp
@@ -1151,10 +1151,12 @@ _Underflow<char, char_traits<char> >::_M_doit (basic_filebuf<char, char_traits<c
       && __this->_M_always_noconv
       && __this->_M_base._M_in_binary_mode()) {
     // If we've mmapped part of the file already, then unmap it.
-    if (__this->_M_mmap_base)
+    if (__this->_M_mmap_base) {
       __this->_M_base._M_unmap(__this->_M_mmap_base, __this->_M_mmap_len);
-    __this->_M_mmap_base = 0;
-    __this->_M_mmap_len = 0;
+      __this->_M_mmap_base = 0;
+      __this->_M_mmap_len = 0;
+      __this->setg(__STATIC_CAST(char*, 0), __STATIC_CAST(char*, 0), __STATIC_CAST(char*, 0));
+    }
 
     // Determine the position where we start mapping.  It has to be
     // a multiple of the page size.
@@ -1175,11 +1177,8 @@ _Underflow<char, char_traits<char> >::_M_doit (basic_filebuf<char, char_traits<c
                      (char*) __this->_M_mmap_base + __STATIC_CAST(ptrdiff_t, __remainder),
                      (char*) __this->_M_mmap_base + __STATIC_CAST(ptrdiff_t, __this->_M_mmap_len));
         return traits_type::to_int_type(*__this->gptr());
-      }
-    } else /* size > 0 ... */ {
-      // There is nothing to map. We unmapped the file above, now zap pointers.
-      __this->_M_mmap_base = 0;
-      __this->_M_mmap_len = 0;
+      } else
+        __this->_M_mmap_len = 0;
     }
   }
 
