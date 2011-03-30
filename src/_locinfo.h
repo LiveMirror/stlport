8bfc95d53e4e093c1806660a57eb89a33b5e0b01
Boris Fomitchev <boris@stlport.com>
2011-03-29 21:03:15 -0700

    locinfo changes

---
+++ src/_locinfo.h
@@ -0,0 +1,11 @@
+namespace std
+{
+  class _Locinfo {
+  public:  
+    _Locinfo(const char *__name = "C");
+    _Locinfo(int _I, const char *__name);
+    _Locinfo(const string& __name);
+    ~_Locinfo();
+    
+  };
+}
